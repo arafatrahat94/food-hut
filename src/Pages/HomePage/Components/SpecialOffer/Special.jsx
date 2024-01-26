71b3c7d44da3d24d1ea81c173a2ba5bbc95ebdc2
arafatrahat94 <arafatrahat94@gmail.com>
2024-01-26 11:47:51 +0600

    readme updated

--- src/Pages/HomePage/Components/SpecialOffer/Special.jsx
+++ src/Pages/HomePage/Components/SpecialOffer/Special.jsx
@@ -3,7 +3,7 @@ import SwipeCardSection from "./SwipeCardSection";
 const Special = () => {
   return (
     <div id="Special">
-      <h1 className="md:text-[38px] text-xl font-semibold text-center mt-5 lg:mt-[120px] mb-5">
+      <h1 className="md:text-[38px] text-3xl font-semibold text-center mt-5 lg:mt-[120px] mb-5">
         Today's <span className="text-accent">Special</span> Offers
       </h1>
 
